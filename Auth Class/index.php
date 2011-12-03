6644dbeed5443e703d55c9427bbe7449e378a8d3
djhenjin <henjin@live.com>
2011-12-03 09:43:23 -0700

    start of setting up login pages and similar

---
+++ Auth Class/index.php
@@ -0,0 +1,13 @@
+<?php
+require_once("config.php");
+require_once("classes.php");
+
+
+    echo " <form name='login' action='index.php' method='post'> ";
+    echo " Username: <input type='text' name='username'> </br>< /input> ";
+    echo " Password: <input type='text' name='password'> </br> </input> ";
+    echo " <input type='submit' value='Login' /> </br> </form> ";
+
+
+
+?>
