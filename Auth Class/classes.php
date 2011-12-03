550fb8e053b2409e9ee2552442e7d027e16d75b7
djhenjin <henjin@live.com>
2011-12-03 06:22:57 -0700

    foo

--- Auth Class/classes.php
+++ Auth Class/classes.php
@@ -4,21 +4,21 @@ require_once("config.php");
 
 class Authentication
 {
-    function login($credentials)
+    function login($credentials, $randkey)
     {
         global $dbhost, $dbname, $dbuser, $dbpass;
         
         $conn = new PDO('mysql:host=$dbhost;dbname=$dbname', $dbuser, $dbpass );
         $login = $conn->prepare ("SELECT * FROM users where user = :username AND password = :password")
-        $login->bindParam(':username', $credentials['user']);
-        $login->bindParam(':password', $credentials['passhashed']);
-        $result = $login->execute();
-        if(result['1'] == $credentials['user'])
+        $login->bindParam(':username', $credentials['0']);
+        $login->bindParam(':password', $credentials['1']);
+        
+        if($login->rowCount() == 1)
         {
             setcookie("session", $randkey, "0", "/", "testing.thesprocketworld.com");
             $sessionupdate = $conn->prepare ("UPDATE users SET sessionid = ':randkey' WHERE user = :username ");
             $sessionupdate->bindparam(':randkey', $randkey);
-            $sessionupdate->bindparam(':username', $credentials['user']);
+            $sessionupdate->bindparam(':username', $credentials['0']);
             $sessionupdate->execute();
         }
         else
