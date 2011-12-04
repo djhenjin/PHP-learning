d0b01695c47d3f837eb13107cb0592ee6e4f7e86
djhenjin <henjin@live.com>
2011-12-04 07:28:37 -0700

    Start of logging in/registering proccess, forms only currently

--- Auth Class/index.php
+++ Auth Class/index.php
@@ -9,9 +9,23 @@ if($_GET['page'] == 'login')
     echo " Password: <input type=\"text\" name=\"password\"/> </br> ";
     echo " <input type=\"submit\" value=\"Login\" /> </br> </form> ";
 }
+else if($_GET['page'] == 'register')
+{
+    echo "<form name=\"register\" action=\"index.php\" method=\"post\"> ";
+    echo "Desired Username: <input type=\"text\" name=\"username\"/></br> ";
+    echo "Password: <input type=\"text\" name=\"pass1\"/></br> ";
+    echo "Re-confirm Password: <input type=\"text\" name=\"pass2\"/></br> ";
+    echo "Email Address <input type=\"text\" name=\"email\"/></br> ";
+    echo "<input type=\"submit\" value=\"Register\"/> </form></br> ";
+}
 else
 {
-    echo "get not set yet";
+    echo "<a href=\"http://testing.thesprocketworld.com/index.php?page=login\"> Have an account already?</a></br>";
+    echo "<a href=\"http://testing.thesprocketworld.com/index.php?page=register\"> Need an account?</a></br>";
+    
+    
+    
+    
 }
 
 
