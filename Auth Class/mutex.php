7623020408919f7a6b189c9743833bd51c2b1053
TMKCodes <toni@aleprok.eu>
2011-12-06 21:21:22 +0200

    Mutex system to avoid multithreading problems with apache.

--- Auth Class/mutex.php
+++ Auth Class/mutex.php
@@ -4,21 +4,32 @@ require_once("config.php");
 
 class Mutex
 {
-    public function mutexlock($username) 
-    {
-        $locks = file_get_contents(mutex.text);
-        $locks = explode(" ", $locks);
-        foreach($locks as $name);
-        {
-            if($name == $username)
-            {
-                //loop
-            }
-            
-        }
-        
+    private $mutexname;
+    public function __construct($mutexname) {
+        $this->mutexname = $mutexname;
     }
-        
-    
+    public function lock() {
+        $locks = file_get_contents("mutex.locks");
+	while($locked != TRUE) {
+            if(strstr($locks, $this->mutexname) == FALSE) {
+	        $locked = FALSE;	
+	    } else {
+                file_put_contents("mutex.locks", $this->mutexname . "\r\n", FILE_APPEND | LOCK_EX);
+	        $locked = TRUE;
+	    }
+        }
+	return $locked;
+    }
+    public function unlock() {
+        $locks = file_get_contents("mutex.locks");
+        $locks = explode("\r\n", $locks);
+        foreach($locks as $key => $value) {
+            if($value == $this->mutexname) {
+                unset($locks[$key]);
+            }
+        }
+        $locks = implode("\r\n", $locks);
+        return file_put_contents("mutex.locks", $locks, LOCK_EX);
+    }   
 }    
 ?>
