cb916b979c705df47bd7d96f192a7649948be0bc
TMKCodes <toni@aleprok.eu>
2011-12-10 00:21:05 +0200

    Feature: JQuery to update chat

--- Auth Class/chat.php
+++ Auth Class/chat.php
@@ -14,19 +14,20 @@ class Chat
         $stmt1->bindParam(':message', $message);
         $stmt1->bindParam(':user', $user);
         $stmt1->execute();
-        
+        header("Location: index.php"); 
     }
     public function displaychat()
     {
         global $dbhost, $dbname, $dbuser, $dbpass;
         $db = new PDO(  'mysql:host=' . $dbhost . ';dbname=' . $dbname . '', $dbuser, $dbpass);
-        
+        echo "<div id=\"Messages\">";
         foreach($db->query('SELECT * FROM messages ORDER BY id DESC') as $row) 
         {
-            echo "Message: $row[1]  </br>";
-            echo "User: $row[2] </br>";
-            echo "<hr>";
+            echo "Message: $row[1]  </br>\r\n";
+            echo "User: $row[2] </br>\r\n";
+            echo "<hr>\r\n";
         }
+	echo "</div>";
     }
 }    
 ?>
--- Auth Class/index.php
+++ Auth Class/index.php
@@ -1,6 +1,20 @@
 <?php
 header('Refresh: 120');
-
+echo "<html>\r\n"
+$head = "<head>\r\n" .
+	"	<title>Chat</title>\r\n" .
+		// Requires JQuery.js in the same folder as index.php
+	"	<script src=\"jquery.js\"></script>\r\n" .
+	"	<script>\r\n".
+	"		$(document).ready(function() {\r\n".
+        		//ajaxTime.php is called every second to get time from server
+        "		var refreshId = setInterval(function() {\r\n".
+	"			$('#Messages').load('loadMessagesFromDB.php');\r\n".
+	"		}, 30000);\r\n" .
+	"	});\r\n".
+	"	</script>\r\n" .
+	"</head>";
+echo $head;
 ob_start();
 require_once("config.php");
 require_once("authentication.php");
@@ -11,8 +25,8 @@ $chat = new Chat();
 
 if($session->auth($_COOKIE['session'])) 
 {
-	echo " <a href=\"http://testing.thesprocketworld.com/index.php?action=logout\"> Logout</a></br>";
-    echo " <a href=\"http://testing.thesprocketworld.com/index.php\"> Refresh This Page</a></br>";
+    echo " <a href=\"http://testing.thesprocketworld.com/index.php?action=logout\"> Logout</a></br>\r\n";
+    echo " <a href=\"http://testing.thesprocketworld.com/index.php\"> Refresh This Page</a></br>\r\n";
     if($_GET['action'] == 'logout')
     {
         $session->logout();
@@ -20,9 +34,9 @@ if($session->auth($_COOKIE['session']))
     }    
 
     
-    echo "<form name=\"chat\" action=\"index.php\" method=\"post\">";
-    echo "Message: <textarea rows=\"10\" cols=\"40\" wrap=\"physical\" name=\"chatmsg\"></textarea></br>";
-    echo "<input type=\"submit\" value=\"Submit Chat!\"></form>";
+    echo "<form name=\"chat\" action=\"index.php\" method=\"post\">\r\n";
+    echo "Message: <textarea rows=\"10\" cols=\"40\" wrap=\"physical\" name=\"chatmsg\"></textarea></br>\r\n";
+    echo "<input type=\"submit\" value=\"Submit Chat!\"></form>\r\n";
     if(isset($_POST['chatmsg']))
     {
         $message = $_POST['chatmsg'];
@@ -35,24 +49,24 @@ if($session->auth($_COOKIE['session']))
 } 
 else 
 {
-    echo "<a href=\"http://testing.thesprocketworld.com/index.php?page=login\"> Have an account already?</a></br>";
-    echo "<a href=\"http://testing.thesprocketworld.com/index.php?page=register\"> Need an account?</a></br>";
+    echo "<a href=\"http://testing.thesprocketworld.com/index.php?page=login\"> Have an account already?</a></br>\r\n";
+    echo "<a href=\"http://testing.thesprocketworld.com/index.php?page=register\"> Need an account?</a></br>\r\n";
 
     if($_GET['page'] == 'login')
     {
-        echo " <form name=\"login\" action=\"index.php?action=login\" method=\"post\"> ";
-        echo " Username: <input type=\"text\" name=\"username\"/> </br> ";
-        echo " Password: <input type=\"password\" name=\"password\"/> </br> ";
-        echo " <input type=\"submit\" value=\"Login\" /> </br> </form> ";
+        echo " <form name=\"login\" action=\"index.php?action=login\" method=\"post\">\r\n";
+        echo " Username: <input type=\"text\" name=\"username\"/> </br>\r\n";
+        echo " Password: <input type=\"password\" name=\"password\"/> </br>\r\n";
+        echo " <input type=\"submit\" value=\"Login\" /> </br> </form> \r\n";
     }
     else if($_GET['page'] == 'register')
     {
-        echo "<form name=\"register\" action=\"index.php?action=register\" method=\"post\"> ";
-        echo "Desired Username: <input type=\"text\" name=\"username\"/></br> ";
-        echo "Password: <input type=\"password\" name=\"pass1\"/></br> ";
-        echo "Re-confirm Password: <input type=\"password\" name=\"pass2\"/></br> ";
-        echo "Email Address <input type=\"text\" name=\"email\"/></br> ";
-        echo "<input type=\"submit\" value=\"Register\"/> </form></br> ";
+        echo "<form name=\"register\" action=\"index.php?action=register\" method=\"post\"> \r\n";
+        echo "Desired Username: <input type=\"text\" name=\"username\"/></br> \r\n";
+        echo "Password: <input type=\"password\" name=\"pass1\"/></br> \r\n";
+        echo "Re-confirm Password: <input type=\"password\" name=\"pass2\"/></br> \r\n";
+        echo "Email Address <input type=\"text\" name=\"email\"/></br> \r\n";
+        echo "<input type=\"submit\" value=\"Register\"/> </form></br> \r\n";
     }
     else if($_GET['action'] == 'register')
     {
@@ -61,11 +75,11 @@ else
         $usrinfo['2'] = $_POST['email'];
         if($session->register($usrinfo))
         {
-            echo "Thank you for registering!";
+            echo "Thank you for registering!\r\n";
         }
         else
         {
-            echo "Please <a href=\"testing.thesprocketworld.com/index.php?page=register\"> Try Again</a>";
+            echo "Please <a href=\"testing.thesprocketworld.com/index.php?page=register\"> Try Again</a>\r\n";
         }
         
     }
@@ -79,8 +93,8 @@ else
         }
         else
         {
-            echo "A Problem occured, Please <a href=\"http://testing.thesprocketworld.com/index.php?page=login\"> Try again</a></br>";
-            echo "Or check your email for the Activation Link.";
+            echo "A Problem occured, Please <a href=\"http://testing.thesprocketworld.com/index.php?page=login\"> Try again</a></br>\r\n";
+            echo "Or check your email for the Activation Link.\r\n";
         }
     
     }
@@ -89,24 +103,17 @@ else
         
         if($session->checkemailconf($_GET['activation']))
         {
-            echo "Your account has been activated";
+            echo "Your account has been activated\r\n";
         }
         else
         {
-            echo "Something went wrong, Please try that again or contact the administrator";
+            echo "Something went wrong, Please try that again or contact the administrator\r\n";
         }
     
     }
     
 
 }
-
-
-
-
-
-
-
-
+echo "</html>";
 ob_end_flush();
 ?>
