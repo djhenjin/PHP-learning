3c8a590edddd1cb18e66676366f4f087cd94d89e
David <henjin@live.com>
2016-02-08 21:19:45 -0700

    switch case swapped for subrstring indexing.
    used mb_substr() to avoid issues with utf8 or other multibyte string encodings. Would have used simple array indexing on a string however this is more compatible in long term

--- Auth Class/authentication.php
+++ Auth Class/authentication.php
@@ -146,77 +146,14 @@ class Authentication
     }
     public function randkey()
     {
-        
+        $options = "abcdefghijklmnopqrstuvwxyz0123456789";
         for($i = 0; $i < 20; $i++) 
         { 
-            $nums[$i] = mt_rand(1, 30); 
+            $nums[$i] = mt_rand(0, 35); 
         } 
             for($i = 0; $i < 20; $i++) 
         { 
-            switch($nums[$i]) 
-            { 
-                case 1: $key[$i] = 'a'; 
-                break;
-                case 2: $key[$i] = 'b';
-                break;
-                case 3: $key[$i] = 'c';
-                break;
-                case 4: $key[$i] = 'd';
-                break;
-                case 5: $key[$i] = 'e'; 
-                break;
-                case 6: $key[$i] = 'f';
-                break;
-                case 7: $key[$i] = 'g';
-                break;
-                case 8: $key[$i] = 'h';
-                break;
-                case 9: $key[$i] = 'i';
-                break;
-                case 10: $key[$i] = 'j';
-                break;
-                case 11: $key[$i] = 'k'; 
-                break;
-                case 12: $key[$i] = 'l';
-                break;
-                case 13: $key[$i] = 'm';
-                break;
-                case 14: $key[$i] = 'n';
-                break;
-                case 15: $key[$i] = 'o'; 
-                break;
-                case 16: $key[$i] = 'p';
-                break;
-                case 17: $key[$i] = 'q';
-                break;
-                case 18: $key[$i] = 'r';
-                break;
-                case 19: $key[$i] = 's';
-                break;
-                case 20: $key[$i] = 't';
-                break;
-                case 21: $key[$i] = 'u'; 
-                break;
-                case 22: $key[$i] = 'v';
-                break;
-                case 23: $key[$i] = 'w';
-                break;
-                case 24: $key[$i] = 'x';
-                break;
-                case 25: $key[$i] = 'y'; 
-                break;
-                case 26: $key[$i] = 'z';
-                break;
-                case 27: $key[$i] = '0';
-                break;
-                case 28: $key[$i] = '1';
-                break;
-                case 29: $key[$i] = '2';
-                break;
-                case 30: $key[$i] = '3';
-                break;
-                
-            }
+            $key[$i] = mb_substr($options, $i, ($i + 1));
         }
         $key = implode($key);
         return $key;
